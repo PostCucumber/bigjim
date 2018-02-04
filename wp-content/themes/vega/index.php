0e516eee8f35e6ae0c16568eb110edbc86600238
Dylan <johnston94@gmail.com>
2018-02-04 01:17:00 -0600

    fbfeed frontend

--- wp-content/themes/vega/index.php
+++ wp-content/themes/vega/index.php
@@ -3,37 +3,19 @@ use KeriganSolutions\FacebookFeed\FacebookFeed;
 $feed    = new FacebookFeed();
 $results = $feed->fetch(9);
 
-if (have_posts()) :
-if (is_home() && !is_front_page()) :
-endif;
 get_header();
 get_template_part('parts/banner'); ?>
-    <div id="content" class="section page-content">
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
+<div id="content" class="section page-content">
+    <div class="container">
+        <div class="flex-row row">
             <?php
-            //echo '<pre>';
-            //print_r($results);
-            //echo '</pre>';
-            ?>            
-            <div class="blog-container">
-                <div class="container wide">
-                    <div class="row">
-                        <?php
-                        // echo '<pre>';
-                        // print_r($results);
-                        // echo '</pre>';
-                        foreach ($results->posts as $result) {
-                            include(locate_template('template-parts/partials/mini-article.php'));
-                        }
-                        ?>
-                        <?php else :
-                            get_template_part('template-parts/content', 'none');
-                        endif; ?>
-                    </div>
-                </div>
-            </div>
-        </main><!-- #main -->
+            // echo '<pre>';
+            // print_r($results);
+            // echo '</pre>';
+            foreach ($results->posts as $result) {
+                include(locate_template('template-parts/partials/mini-article.php'));
+            } ?>
+        </div>
     </div>
-<?php
-get_footer(); ?>
+</div>
+<?php get_footer(); ?>
--- wp-content/themes/vega/template-parts/partials/mini-article.php
+++ wp-content/themes/vega/template-parts/partials/mini-article.php
@@ -1,55 +1,64 @@
-<!DOCTYPE html>
-<div class="col-sm-6 col-md-4 col-lg-4 text-center">
-    <div class="blog-article">
-        <?php
-        if($result->type != 'status') {
-            if($result->type == 'video') { ?>
-                <div class="embed-responsive embed-responsive-16by9">
-                    <iframe
-                        src="<?php echo $result->link ?>"
-                        style="border:none;overflow:hidden"
-                        scrolling="no"
-                        frameborder="0"
-                        allowTransparency="true"
-                        allowFullScreen="true"
-                        class="embed-responsive-item img-fluid border-bottom"
-                        width="100%"
-                        height="170">
-                    </iframe>
-                    <?php echo $content; ?>
-                </div>
-            <?php }
-            elseif($result->type == 'photo' || $result->type =='link') { ?>
-                <div class="embed-responsive embed-responsive-16by9">
-                    <div class="fbblog-photo">
-                        <img src="<?php echo $result->full_picture; ?>" alt="<?php echo $result->caption; ?>" >
-                    </div>
-                </div>
-            <?php } ?>
-            
-            <div class="fbblog-msg">
+<link rel="stylesheet" href="card.css" type="text/css">
+<link rel="stylesheet" href="tooltip.css" type="text/css">
+<div class="col-xs-6 col-sm-4 col-lg-3">
+    <?php
+    if($result->message == NULL)
+        $message = 'From the walk...';
+    else
+        $message = wp_trim_words($result->message, $num_words = 22, $more = '...' );
+
+    switch ($result->type) {
+        case "video": ?>
+            <div class="thumbnail" style="padding:0px;">
+                <iframe
+                    src="<?php echo $result->link; ?>"
+                    scrolling="no"
+                    frameborder="0"
+                    allowTransparency="true"
+                    allowFullScreen="true">
+                </iframe>
+                <div class="caption">
+                    <p class="flex-text text-muted">
+                    <?php echo $message; ?>
+                    </p>
+                    <a class="btn btn-primary-custom" href="<?php echo $result->link; ?>">Facebook</a>
+                    <a class="btn btn-inverse" href="https://www.facebook.com/BigJimsWalk/videos?lst=1792038472%3A100011444923852%3A1517723652" target="_blank">More Videos</a>    
+                </div>          
+            </div>
             <?php
-                //echo $result->type;
-                wp_trim_words( $content, $num_words = 22, '...' );
-                echo $content;
-            ?>
-            </div>
-            <div class="blog-link"> 
-            <a class="btn btn-primary-custom btn-readmore" href="<?php echo $result->permalink_url; ?>" target="_blank" >
-                View Post
-            </a>
-        </div>
+            break;
 
-        <div class="time-posted">
-            <p><?php echo $dateposted ?></p>    
-        </div>
-
-
-        <?php }
-        elseif($result->type == 'status') { ?>
-            <div class="fbblog-status">
+        case "status": ?>
+            <div class="thumbnail fbblog-status">
                 <p><?php echo $result->message; ?></p>
+                <a class="btn btn-primary-custom" href="<?php echo $result->link ?>" target="_blank">View Post</a>
             </div>
-        <?php } ?> 
-    </div>
+            <?php
+            break;
+
+        case "link": ?>
+            <div class="thumbnail" style="border:none">
+                <img src="<?php echo $result->full_picture; ?>" class="img img-responsive" alt="<?php echo $result->caption; ?>">
+                <div class="caption">
+                    <p class="flex-text text-muted">Big Jim Shared a Link.</p>
+                    <a class="btn btn-primary-custom" href="<?php echo $result->link ?>" target="_blank" style="float:left;width:50%;">
+                    Open Link
+                    </a>
+                    <a class="btn btn-inverse" href="<?php echo $result->permalink_url ?>" target="_blank" style="width:50%;">View Post</a> 
+                </div>
+            </div>
+            <?php
+            break;
+            
+        case "photo": ?>
+            <div class="fbblog-photo">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <img src="<?php echo $result->full_picture; ?>" alt="<?php echo $result->caption; ?>" >
+                </div>
+            </div>
+            <?php
+            break;
+
+        default:
+    } ?>
 </div>
